159f3ed155c154ca262135eed94f843c1180979c
Luizeduardovae <duduvivas18@hotmail.com>
2023-06-02 23:44:01 -0300

    feat/FuncaoSaque

--- Desafio.py
+++ Desafio.py
@@ -14,12 +14,28 @@ sim_nao = """
 
 => """
 
-
 saldo = 0
 limite = 500
 extrato = []
 numero_saques = 0
 LIMITE_SAQUES = 3
+
+def saques(*, saque, numero_saques, saldo, extrato):
+    
+    if saque > saldo:
+        print("\nSaldo Insuficiente")
+    elif saque > limite:
+        print("\nLimite Insuficiente")
+    elif numero_saques >= LIMITE_SAQUES:
+        print("""\nQuantidade de saques diários atingidos
+                  \nSelecione outra opção!
+             """)
+    else:
+        saldo -= saque
+        numero_saques += 1
+        extrato.append(f"Saque: {saque}")
+    return saldo, numero_saques
+        
 
 while True:
 
@@ -27,7 +43,7 @@ while True:
     
     if opcao == "d":
         while True:
-            print( " \n Deseja depositar?")
+            print( " \nDeseja depositar?")
             yes_no = input(sim_nao)
 
             if yes_no == "y":
@@ -38,35 +54,18 @@ while True:
                 break
 
     elif opcao == "s":
-        if numero_saques < LIMITE_SAQUES:
+            
             while True:
                 print( "\nDeseja Sacar?")
                 yes_no = input(sim_nao)
                 saque = 0
-            
-                if numero_saques < LIMITE_SAQUES:
-                    if yes_no == "y":
-                        saque = float(input("Valor do saque:  "))
-                        if saque > saldo:
-                            print("\nSaldo Insuficiente")
-                        elif saque > 500:
-                            print("\nLimite Insuficiente")
-                        else:
-                            saldo -= saque
-                            numero_saques += 1
-                            extrato.append(f"Saque: {saque}")
-                    else:
-                        break
+                
+                if yes_no == "y":
+                    saque = float(input("Valor do saque:  "))
+                    saldo, numero_saques = saques(saque=saque, numero_saques=numero_saques, saldo=saldo,extrato=extrato)
                 else:
-                    print(""" \nQuantidade de saques diarios atingidos 
-                              \nSelecione outra opcao!
-                        """)
                     break
-        else:
-            print(""" \nQuantidade de saques diarios atingidos 
-                      \nSelecione outra opcao!
-                 """)
-            continue
+
 
     elif opcao == "e":
         print("\nExtrato")
